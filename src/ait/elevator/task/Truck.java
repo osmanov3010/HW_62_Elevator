6953d0a25531a2b1865e23b77258c01b4cb90033
Osmanov Yakub <osmanov3010@gmail.com>
2023-12-08 12:26:47 +0100

    some fixes

--- src/ait/elevator/task/Truck.java
+++ src/ait/elevator/task/Truck.java
@@ -2,8 +2,18 @@ package ait.elevator.task;
 
 import ait.elevator.model.Elevator;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class Truck implements Runnable {
-    private static Object monitor = new Object();
+    private static Object[] monitor = {new Object(), new Object(), new Object(), new Object()};
+    // идея такая, что для каждого элеватора сделать свой объект
+    // костыль, но я предположу, что если бы можно было править класс Elevator,
+    // то можно было бы задать equals и hashCode Elevator и в траках сделать MAP для всех элеваторов.
+    // В конструкторе трака добавлять элеватор в эту мапу и дальше использовать как объект для синхронизации.
+    // Не знаю как бы это влияло на производительность :)
     private int nRaces;
     private int capacity;
     private Elevator[] elevators;
@@ -12,18 +22,18 @@ public class Truck implements Runnable {
         this.nRaces = nRaces;
         this.capacity = capacity;
         this.elevators = elevator;
+
     }
 
     @Override
     public void run() {
         for (int i = 0; i < nRaces; i++) {
             int capacityForEachElevator = capacity / elevators.length; // It will work only for even numbers of capacity and elevator count
-            for (Elevator el : elevators) {
-                synchronized (monitor) {
-                    el.add(capacityForEachElevator);
+            for (int j = 0; j < elevators.length; j++) {
+                synchronized (monitor[j]) {
+                    elevators[j].add(capacityForEachElevator);
                 }
             }
-
         }
     }
 }
